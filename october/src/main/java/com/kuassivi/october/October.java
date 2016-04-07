a2a83ea229165725ef5f98cdb841a8294a4b75e9
Francisco Gonzalez <riadigos.fran@gmail.com>
2016-04-07 13:18:11 +0200

    Fixed October Component Interface.

--- october/src/main/java/com/kuassivi/october/October.java
+++ october/src/main/java/com/kuassivi/october/October.java
@@ -1,7 +1,5 @@
 package com.kuassivi.october;
 
-import com.kuassivi.october.annotation.ApplicationComponent;
-
 import android.app.Application;
 
 import java.lang.reflect.InvocationTargetException;
@@ -11,18 +9,12 @@ public class October {
     private static OctoberComponent component;
 
     public static void initialize(Application application) {
-        //noinspection ReflectionForUnavailableAnnotation
-        if (!application.getClass().isAnnotationPresent(ApplicationComponent.class)) {
-            throw new RuntimeException(String.format("%s class is not annotated with @%s",
-                                                     application.getClass().getSimpleName(),
-                                                     ApplicationComponent.class.getSimpleName()));
-        }
-
         //noinspection TryWithIdenticalCatches
         try {
             Class<?> clazz = Class.forName(Config.OCTOBER_CLASS_NAME);
             OctoberComponentInitializer initializer =
                     (OctoberComponentInitializer) clazz.getConstructor().newInstance();
+            //noinspection unchecked
             component = initializer.initialize(application);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Something unexpected was happened. "
